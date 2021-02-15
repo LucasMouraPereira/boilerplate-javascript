209c68c911a62219afa448b3d0a358318bbc0024
Lucas Moura <lucas@ioasys.com.br>
2021-02-15 18:06:20 -0300

    first commit

---
+++ src/server/routes.js
@@ -0,0 +1,8 @@
+const Routes = require("next-routes");
+
+const { HOME } = require("../utils/constants/urls");
+
+module.exports = Routes()
+    //Home
+    .add({ name: "Home", pattern: HOME, page: "Home" })
+
